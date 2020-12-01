180372f57fd386c36af9c0c32cfc2db25f3ef4b7
Arthur Nicholson-Gumuła <arthur@cloudawesome.uk>
2020-12-01 18:42:32 +0000

    Adds Associate extenion methods tests; COB

---
+++ src/CloudAwesome.Xrm.Core/CloudAwesome.Xrm.Core.Tests/EntityExtensionsTests/AssociateTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using NUnit.Framework;
+
+namespace CloudAwesome.Xrm.Core.Tests.EntityExtensionsTests
+{
+    [TestFixture]
+    public class AssociateTests: BaseFakeXrmTest
+    {
+        [Test]
+        public void BasicAssociateTest()
+        {
+            var context = new XrmFakedContext();
+            var orgService = context.GetOrganizationService();
+            context.AddRelationship("account_primary_contact", new XrmFakedRelationship()
+            {
+                RelationshipType = XrmFakedRelationship.enmFakeRelationshipType.OneToMany,
+                Entity1LogicalName = "contact",
+                Entity1Attribute = "contactid",
+                Entity2LogicalName = "account",
+                Entity2Attribute = "contactid"
+            });
+
+            context.Initialize(new List<Entity>() {
+                TestAccount1,
+                TestAccount1Duplicate
+            });
+
+            var contact = new Contact()
+            {
+                FirstName = "Jaroslaw",
+                LastName = "Czyz"
+            };
+            contact.Create(orgService);
+
+            var accountCollection = new EntityReferenceCollection()
+            {
+                TestAccount1.ToEntityReference(),
+                TestAccount1Duplicate.ToEntityReference()
+            };
+
+            Assert.DoesNotThrow(() =>
+                contact.Associate(orgService, "account_primary_contact", accountCollection));
+
+        }
+
+        [Test]
+        public void BasicAssociateTestWithEnuerableRecords()
+        {
+            var context = new XrmFakedContext();
+            var orgService = context.GetOrganizationService();
+            context.AddRelationship("account_primary_contact", new XrmFakedRelationship()
+            {
+                RelationshipType = XrmFakedRelationship.enmFakeRelationshipType.OneToMany,
+                Entity1LogicalName = "contact",
+                Entity1Attribute = "contactid",
+                Entity2LogicalName = "account",
+                Entity2Attribute = "contactid"
+            });
+
+            context.Initialize(new List<Entity>() {
+                TestAccount1,
+                TestAccount1Duplicate
+            });
+
+            var contact = new Contact()
+            {
+                FirstName = "Jaroslaw",
+                LastName = "Czyz"
+            };
+            contact.Create(orgService);
+
+            IEnumerable<Account> accountCollection = new List<Account>()
+            {
+                TestAccount1,
+                TestAccount1Duplicate
+            };
+
+            Assert.DoesNotThrow(() =>
+                contact.Associate(orgService, "account_primary_contact", accountCollection));
+
+        }
+    }
+}
