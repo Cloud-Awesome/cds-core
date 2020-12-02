8f0e43d3763e5c18c7cc301e66f78851974d1010
Arthur Nicholson-Gumuła <arthur@cloudawesome.uk>
2020-12-02 02:42:32 +0000

    Delete single record unit tests

--- src/CloudAwesome.Xrm.Core/CloudAwesome.Xrm.Core.Tests/QueryHelperTests/DeleteAllResultsTests.cs
+++ src/CloudAwesome.Xrm.Core/CloudAwesome.Xrm.Core.Tests/QueryHelperTests/DeleteAllResultsTests.cs
@@ -8,6 +8,13 @@ namespace CloudAwesome.Xrm.Core.Tests.QueryHelperTests
 {
     public class DeleteAllResultsTests
     {
+        // TODO - 5 * DeleteAllResultsTests to write
+
+        // 1. Delete all - happy path
+        // 2. Too many records to delete, throw exception
+        // 3. Correct number - delete all
+        // 4. Unexpected number retrieved - delete 0, throw exception
+        // 5. No records returned - do nothing
 
     }
 }
--- src/CloudAwesome.Xrm.Core/CloudAwesome.Xrm.Core.Tests/QueryHelperTests/DeleteSingleRecordTests.cs
+++ src/CloudAwesome.Xrm.Core/CloudAwesome.Xrm.Core.Tests/QueryHelperTests/DeleteSingleRecordTests.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using NUnit.Framework;
 
 namespace CloudAwesome.Xrm.Core.Tests.QueryHelperTests
 {
-    public class DeleteSingleRecordTests
+    [TestFixture]
+    public class DeleteSingleRecordTests: BaseFakeXrmTest
     {
+        // TODO - What about if the query returns no results?
 
+        [Test]
+        public void DeleteSingleRecordTest()
+        {
+            var context = new XrmFakedContext();
+            var orgService = context.GetOrganizationService();
+
+            context.Initialize(new List<Entity>() {
+                TestAccount1
+            });
+
+            var preAccounts = (from a in context.CreateQuery<Account>()
+                select a).ToList();
+
+            Assert.AreEqual(1, preAccounts.Count);
+            Assert.DoesNotThrow(
+                () => SampleQueryExpression.DeleteSingleRecord(orgService));
+
+            var postAccounts = (from a in context.CreateQuery<Account>()
+                select a).ToList();
+
+            Assert.AreEqual(0, postAccounts.Count);
+        }
     }
 }
